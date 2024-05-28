7ad26fd21ba905dce6e88f469bf9ea5fc4293ab5
rftq <burdaet@gmail.com>
2024-05-28 21:31:12 +0300

    update player and add images

--- lesson/player.js
+++ lesson/player.js
@@ -8,14 +8,14 @@ let playlist = {
     },
     tracks: [
         {
-            coverImageUrl: 'track1.png',
+            coverImageUrl: 'track1.jpeg',
             artistName: 'Eminem',
             title: 'Rap God',
             fileUrl: 'Eminem_-_Rap_God.mp3',
             isHot: false,
         },
         {
-            coverImageUrl: 'track2.png',
+            coverImageUrl: 'track2.jpeg',
             artistName: '50cent',
             title: 'In da Club',
             fileUrl: '50_Cent_-_In_Da_Club.mp3',
@@ -38,7 +38,7 @@ function renderPlaylist(playlistForRendering) {
 }
 
 
-function renderPlaylistHeader(playlistForRendering){
+function renderPlaylistHeader(playlistForRendering) {
     // здесь логика отрисовки шапки "входного" плейлиста
     let playlistImageElement = document.createElement('img');
     playlistImageElement.src = playlistForRendering.coverImageUrl;
@@ -47,12 +47,22 @@ function renderPlaylistHeader(playlistForRendering){
     let playlistTitleElement = document.createElement('h2');
     playlistTitleElement.append(playlistForRendering.title);
     document.body.append(playlistTitleElement);
-  }
+}
 
 
-  function renderTrack(inputTrackForRendering){
+function renderTrack(inputTrackForRendering) {
     // здесь логика отрисовки "входного" трека
     let trackElement = document.createElement('div');
     trackElement.append(inputTrackForRendering.artistName + ' - ' + inputTrackForRendering.title);
+
+    let playerElement = document.createElement('audio');
+    playerElement.src = inputTrackForRendering.fileUrl;
+    playerElement.controls = true;
+    trackElement.append(playerElement);
+
+    let coverElement = document.createElement('img');
+    coverElement.src = inputTrackForRendering.coverImageUrl;
+    trackElement.append(coverElement);
+
     document.body.append(trackElement);
-  } 
+}
